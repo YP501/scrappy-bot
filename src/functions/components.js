e865d2b56b0165c968a18f35786c443725ee4e14
YP501 <itzypboi@gmail.com>
2023-09-30 01:42:55 +0200

    [+] Change
    Changed the export on this file

--- src/functions/components.js
+++ src/functions/components.js
@@ -1,10 +1,8 @@
 import { ActionRowBuilder, ButtonBuilder } from "discord.js";
 
-function disableButtons(row) {
+export function disableButtons(row) {
   const buttons = [];
   const buttonsJson = row.components;
   buttonsJson.forEach((button) => buttons.push(ButtonBuilder.from(button).setDisabled(true)));
   return new ActionRowBuilder().addComponents(buttons);
 }
-
-export { disableButtons };
